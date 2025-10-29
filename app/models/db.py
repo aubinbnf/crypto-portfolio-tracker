68d0b2ce8e36a933f31999e589abfeffef689597
aubin <aubin.bonnefoy25@hotmail.fr>
2025-10-29 18:55:58 +0100

    Data manage in an external docker volume

--- app/models/db.py
+++ app/models/db.py
@@ -1,10 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data/portfolio.db"
+load_dotenv()
+
+sqlachemy_database_url = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    sqlachemy_database_url, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
